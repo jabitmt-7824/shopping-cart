84a2548b41478c13ed202b7bb9def663fc8e0ac5
Jabir MT <jabirmt7824@gmail.com>
2020-07-18 16:12:27 +0530

    buttons and states are added

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,30 @@
 import React from 'react'
 
 class CartItem extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            title: "Mobile Phone",
+            price: 999,
+            qnty: 1,
+            img: ""
+        }
+    }
     render(){
+        const {title, price, qnty} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
-                    <img alt="phone" style={Styles.image}/>
+                    <img style={Styles.image} alt="phone"/>
                 </div>
                 <div className="right-block">
-                    <div style={ {fontSize:25} }>Phone</div>
-                    <div style={ {fontSize:25} }>Rs 2199</div>
-                    <div style={ {fontSize:25} }>Qty: 1</div>
+                    <div style={ {fontSize:25} }>{title}</div>
+                    <div style={ {fontSize:15} }>Rs {price}</div>
+                    <div style={ {fontSize:15} }>Qty: {qnty}</div>
                     <div className="cart-item-actions">
-
+                        <img alt="incease-button" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992651.svg" />
+                        <img alt="decrease-button" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992683.svg"/>
+                        <img alt="delete-button" className="action-icons" src="https://image.flaticon.com/icons/svg/3096/3096673.svg"/>
                     </div>
                 </div>
 
@@ -23,9 +35,10 @@ class CartItem extends React.Component {
 
 const Styles = {
     image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4
+        height: 140,
+        width: 150,
+        borderRadius: 4,
+        // backgroundColor: "#ccc"
     }
 }
 
